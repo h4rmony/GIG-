a380f0b791ba82918fef2c5b7245f671867e0fa7
Voltaire Stephen Medina <voltmedina@gmail.com>
2017-03-16 23:29:48 -0400

    Added basic user input, structured basis of "sieve" implementation in comments

--- src/voltaire/medina/driver.java
+++ src/voltaire/medina/driver.java
@@ -1,5 +1,29 @@
 package voltaire.medina;
+import java.util.*;
 
 public class driver {
 
+	public static void main (String args[]){
+		
+		Scanner keyboard = new Scanner (System.in);
+		
+		String userBOOL; 
+		int userCHOICE;
+		
+		System.out.println("Take in simple user imput, yes | no ");
+		
+		userBOOL = keyboard.nextLine();
+		
+		System.out.println("Take in simple user input, \n press 1 for A\n press 2 for B\n press 3 for C");
+		
+		userCHOICE = keyboard.nextInt();
+		
+		// use a sieve implementation. 
+		// sieve out the database choices until x amount of combinations remain OR 
+		// 		x amount of questions have been asked
+		// if "user says no" {
+		//		SieveAction(1)
+		// }
+	}
+	
 }
