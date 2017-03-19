90311eff2dd65a5cebc441f0a3ec79cf19a4768a
Voltaire Stephen Medina <voltmedina@gmail.com>
2017-03-18 20:17:59 -0400

    Structured and added database file

---
+++ src/voltaire/medina/outputDB.java
@@ -0,0 +1,17 @@
+package voltaire.medina;
+
+public class outputDB {
+
+	// main nouns
+	String sampleName;
+	
+	// main 2nd nouns
+	String sample2ndName;
+	
+	// unique names
+	String sampleUniqueName;
+	
+	// name adjectives
+	String sampleAdj;
+	
+}
